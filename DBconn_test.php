e60b6685ad58499c598d9fcbadf501ce8f7fbb8b
Chris Voorberg <chris.voorberg@gmail.com>
2013-02-23 14:20:01 -0500

    Add caselaw Version 1
    articlepage.php is a complete PHP-include driven template. Lists and
    Inserts live to/from the Database

---
+++ DBconn_test.php
@@ -0,0 +1,28 @@
+<?php
+
+class DBconn{
+    
+private $hostname = "mysql2.cloudsites.gearhost.com";
+private $username = "cumulus";
+private $dbname = "cumulus";
+
+//These variable values need to be changed by you before deploying
+private $password = "kbvEigBt";
+
+public function getConn(){
+
+    try {
+    $db = new PDO('mysql:host=mysql2.cloudsites.gearhost.com;dbname=cumulus', $this->username, $this->password);
+    
+        } 
+        catch (PDOException $e) {
+            $error_message = $e->getMessage();
+            echo "failed to connect";
+            exit();
+        }
+    return $db;
+    }
+    
+}
+
+?>
