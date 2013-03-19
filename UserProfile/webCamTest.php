86d800c81027d0e0a5581cbe19f4d7618b204099
Nnabugwu <nnabugwuezema@gmail.com>
2013-03-19 08:05:08 -0400

    current state of features

---
+++ UserProfile/webCamTest.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Web cam test</title>
+
+<script language="javascript" src="js/jquery.js"></script>
+<script language="javascript" src="jpegcam/htdocs/webcam.js"></script>
+<script language="javascript" src="jpegcam/htdocs/webcam.js"></script>
+<script language="javascript" src="js/profile.js"></script>
+
+</head>
+
+<body>
+<div id="camera"></div><!--/camera-->
+
+<!--<div id="uploads"></div>--><!--/uploads-->
+
+<a href="javascript:void(webcam.snap())">Take Snapshot</a>
+<a href="javascript:void(webcam.configure('camera'))">Configure</a>
+
+</body>
+</html>
