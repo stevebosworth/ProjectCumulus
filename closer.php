484b3cbfa6c5d9cd00a2adcb704bb83f170f87ac
Chris Voorberg <chris.voorberg@gmail.com>
2013-03-18 20:30:04 -0400

    Working feature PRE conversion to Object-Oriented Programming

--- closer.php
+++ closer.php
@@ -4,7 +4,7 @@ echo
 "</div> <!-- /container -->
 
     
-    <script>window.jQuery || document.write('<script src='js/vendor/jquery-1.9.0.min.js'><\/script>')</script>
+    <!--<script>window.jQuery || document.write('<script src='js/vendor/jquery-1.9.0.min.js'><\/script>')</script>-->
     <script src='js/plugins.js'></script>
     <script src='js/main.js'></script>
 
