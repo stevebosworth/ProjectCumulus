0c4256441f24b190bbd27ef90a7f5795f236773c
Chris Voorberg <chris.voorberg@gmail.com>
2013-04-20 11:49:53 -0400

    Updated head.inc.php by removing <head> tags
    When using head.inc.php with an include, wrap it in <head> tags and add
    a <title> and any other necessary references.

--- include/head.inc.php
+++ include/head.inc.php
@@ -4,15 +4,11 @@ echo
 <!--[if lt IE 7]>      <html class='no-js lt-ie9 lt-ie8 lt-ie7'> <![endif]-->
 <!--[if IE 7]>         <html class='no-js lt-ie9 lt-ie8'> <![endif]-->
 <!--[if IE 8]>         <html class='no-js lt-ie9'> <![endif]-->
-<!--[if gt IE 8]><!--> <html class='no-js'> <!--<![endif]-->
-<head>
+<!--[if gt IE 8]>      <html class='no-js'> <![endif]-->
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
-    <title>Project Cumulus Home Page</title>
     <meta name='description' content=''>
     <meta name='viewport' content='width=device-width'>
-
-    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
     <link rel='stylesheet' href='css/normalize.css'>
     <link rel='stylesheet' href='css/main.css'>
@@ -20,8 +16,7 @@ echo
     <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
     <script src='js/vendor/modernizr-2.6.2.min.js'></script>
     <script src='http://use.edgefonts.net/arvo.js'></script>
-    <!-- Last edited by Chris Voorberg - February 9 2013 -->
-</head>
+    <!-- Last edited by Chris Voorberg - April 20 2013 -->
 ";
 
 ?>
--- section.php
+++ section.php
@@ -32,27 +32,14 @@
 ?> <!-- /requires -->
 
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Project Cumulus Home Page</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
+    <?php include 'include/head.inc.php'; ?>
+
+    <!-- Additional files for the head section -->
+    <title>Project Cumulus - Section <?php echo $sec_num; ?></title>
+    <link rel="stylesheet" href="css/jquery-ui-1.10.1.custom.css">
 
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
-    <link rel="stylesheet" href="css/jquery-ui-1.10.1.custom.css">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/article.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    <script src="http://use.edgefonts.net/arvo.js"></script>
-    <!-- Last edited by Chris Voorberg - February 9 2013 -->
 </head>
 <body>
     <!--[if lt IE 7]>
