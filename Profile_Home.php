643522b5a447f77b44a70667de4ee498edcfff20
Nnabugwu <nnabugwuezema@gmail.com>
2013-05-29 23:01:40 -0400

    add

--- Profile_Home.php
+++ Profile_Home.php
@@ -14,7 +14,6 @@ require_once('classes/UserProfileClasses/login_db.class.php');
 require_once('classes/UserProfileClasses/login.class.php');
 
 ?>
-
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
---
+++ classes/UserProfileClasses/form.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Validation Class Example</title>
+</head>
+
+<body>
+<form action="validation.php" method="post">
+<table>
+<tr>
+<td>Name</td>
+<td><input type="text" name="txt_name"/></td>
+</tr>
+<tr>
+<td>Student ID</td>
+<td><input type="text" name="txt_studentID"/></td>
+</tr>
+<tr>
+<td>Email</td>
+<td><input type="text" name="txt_email"/></td>
+</tr>
+<tr>
+<td>Postal Code</td>
+<td><input type="text" name="txt_postalCode"/></td>
+</tr>
+<tr>
+<td>Phone Number</td>
+<td><input type="text" name="txt_phoneNo"/></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" name="btn_submit" value="Submit"/></td>
+</tr>
+</form>
+</body>
+</html>
